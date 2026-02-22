e0765096f2175b3138b8eb3bc3c4534bc38097fe
Dave6375 <idokod09@gmail.com>
2026-02-21 21:27:14 -0600

    Install @radix-ui/react-icons and fix import typo in layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import {HorizontalGradient} from "@/components/marketing/horizontal-gradient";
 import Link from "next/link";
 import { buttonVarients } from "@/components/ui/button";
-import { ArrowLeftIcon } from '@radix-ui/react-icon';
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
 import { FileScriptIcon } from "hugeicons-react";
 
 
