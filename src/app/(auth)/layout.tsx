35e0e19e3e34aa35257632fb4b77b1994fe40f2b
Dave6375 <idokod09@gmail.com>
2026-02-21 21:23:44 -0600

    Install @clerk/nextjs, fix auth pages, and improve horizontal-gradient component

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+import { cn } from "@/lib/utils";
+import {HorizontalGradient} from "@/components/marketing/horizontal-gradient";
+import Link from "next/link";
+import { buttonVarients } from "@/components/ui/button";
+import { ArrowLeftIcon } from '@radix-ui/react-icon';
+import { FileScriptIcon } from "hugeicons-react";
+
+
---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { SignIn, useUser } from "@clerk/nextjs";
+import { experimental__simple, dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
+import { redirect } from "next/navigation"
+
+export const runtime = 'edge';
+export const preferredRegion = 'home';
+
+export default function SignInPage() {
+    const {theme} = useTheme()
+    const { isSignedIn } = useUser();
+
+    if(isSignedIn) {
+        redirect('/chat')
+    }
+    return (
+        <SignIn
+            appearance={{
+                baseTheme: theme === 'dark' ? dark : experimental__simple,
+                variables: {
+                    colorBackground: theme === 'dark' ? '#090909' : '#efefef',
+                    colorInputBackground: 'transparent'
+                }
+            }}
+            />
+    );
+}
