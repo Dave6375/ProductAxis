6f5fa742c86219f173d41d6fae991b0472477d98
Dave6375 <idokod09@gmail.com>
2026-02-27 20:47:10 -0600

    Install @vercel/kv, add Chat type and fix imports in chat.ts

--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -1,3 +1,13 @@
 import { CoreMessage, ToolContent } from 'ai'
 
 export type LLMSelection = `xai:${string}` | `perplexity:${string}`;
+
+export interface Chat extends Record<string, any> {
+    id: string;
+    title: string;
+    createdAt: Date;
+    userId: string;
+    path: string;
+    messages: CoreMessage[];
+    sharePath?: string;
+}
